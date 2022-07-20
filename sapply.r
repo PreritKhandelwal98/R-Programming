b2ae7e102b6ec6dda1202e4e062e4dc7127f7174
Prerit khandelwal <102379304+PreritKhandelwal98@users.noreply.github.com>
2022-07-20 13:44:42 +0530

    Add files via upload
    In this program I have created sapply() function which takes 
    -> The sapply() function in the R Language takes a list, vector, or data frame as input , and 
    -> gives output in the form of an array or matrix object.

---
+++ sapply.r
@@ -0,0 +1,9 @@
+# create sample data
+sample_data<- data.frame( x=c(1,2,3,4,5,6),
+                          y=c(3,2,4,2,34,5))
+print( "original data:")
+sample_data
+
+# apply sapply() function
+print("data after sapply():")
+sapply(sample_data, max)
